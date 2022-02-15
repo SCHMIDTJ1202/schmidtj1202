1dda9217aa7f4627b9d15a407476ee3ecdaf2a6b
SCHMIDTJ1202 <99296375+SCHMIDTJ1202@users.noreply.github.com>
2022-02-16 11:54:35 +1300

    Add files via upload

---
+++ 01_Temperature_Converter/01_Mystery_box_base.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("temperature Converter")
+    something = Converter(root)
+    root.mainloop("Converter")
+    
